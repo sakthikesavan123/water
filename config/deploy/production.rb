bfe86e3ebbc36790ca7397056b1dbb82d7e1145e
sakthikesavan123 <sakthi@codingmart.com>
2018-08-28 19:05:09 +0530

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,14 @@
-set :branch, 'master' # git branch 
-	set :stage, :production
-	set :rails_env, :production
 
-	set :normalize_asset_timestamps, %{public/images public/javascripts
-	public/stylesheets}
+server '18.205.161.230',
+ user: 'ubuntu',
+ roles: %w{app web app},
+ ssh_options: {
+     forward_agent: true,
+ }
 
-	role :app, %w{ubuntu@ec2-18-205-161-230.compute-1.amazonaws.com}    # replace with your server IP
+set :stage, :production
+set :branch, :master
+set :ssh_options, {:forward_agent => true}
 
-	set :ssh_options, {
-	  # keys: %w(/Users/bhuvanamalini/Documents/project_codingmart/dorak_pem_key/dorakholdingdevelopment.pem),
-	  keys: %w(/home/codingmart/Desktop/pem/deploy_test.pem)
-	# keys: %w(/Users/karthi/Documents/keys/dorakholdingdevelopment.pem),
-	}
+# Setting RAILS_ENV environment variable on server
+set :rails_env, :production
